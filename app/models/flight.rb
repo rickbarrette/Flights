f11a218e51d8a71cb612b3d9fb4cbe026c6f5fc4
Rick Barrette <rickbarrette@gmail.com>
2023-07-02 00:52:39 -0400

    Added Date scopes & after_update method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -9,6 +9,13 @@ class Flight < ApplicationRecord
   validates :tach, presence: true
   validates :hobbs, presence: true
 
+  after_update :update_aircraft
+
+  scope :thirty_days, -> { where('date >= ?', Time.now - 30.days) }
+  scope :month, -> { where('date >= ?', Time.now - 1.month) }
+  scope :three_months, -> { where('date >= ?', Time.now - 3.month) }
+  scope :year, -> { where('date >= ?', Time.now - 1.year) }
+    
   # Return a human readbale string
   def to_s
     "#{self.date.to_s} - #{self.from} #{self.to}" 
@@ -31,7 +38,24 @@ class Flight < ApplicationRecord
     time = (self.hobbs - self.prev.hobbs).round(1) 
     return time unless time <= 0
 
-    "#{(self.tach - self.prev.tach).round(1)} (Tach)"
+    (self.tach - self.prev.tach).round(1)
+  end
+
+  # Update aircraft times & oil
+  def update_aircraft
+    t = (tach - prev.tach).round(1)
+
+    # TODO update aircraft TT with increase of tach time
+    # TODO update aircraft engine with tach time
+    # TODO update oil_change with added oil
+    if aircraft.engine.tach < tach
+      aircraft.tt = aircraft.tt + t
+      aircraft.engine.tach = aircraft.engine.tach
+      aircraft.save
+    end
+
+    aircraft.engine.oil_change.added = aircraft.engine.oil_change.added + oil
+    aircraft.engine.oil_change.save
   end
 
 end
