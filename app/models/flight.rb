79752b2e06ac4be3fcba60bdaa6e9b5a12de4786
Rick Barrette <rickbarrette@gmail.com>
2023-07-01 22:42:36 -0400

    Added flight time to the flight.show

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -9,8 +9,29 @@ class Flight < ApplicationRecord
   validates :tach, presence: true
   validates :hobbs, presence: true
 
+  # Return a human readbale string
   def to_s
     "#{self.date.to_s} - #{self.from} #{self.to}" 
   end
-  
+
+  # Return the next flight
+  def next
+    aircraft.flights.where("id > ?", id).first
+  end
+
+  # Returns the previous flight
+  def prev
+    aircraft.flights.where("id < ?", id).last
+  end
+
+  # Returns the time for a flight
+  def time
+    return 0 if self.prev.nil?
+
+    time = (self.hobbs - self.prev.hobbs).round(1) 
+    return time unless time <= 0
+
+    "#{(self.tach - self.prev.tach).round(1)} (Tach)"
+  end
+
 end
